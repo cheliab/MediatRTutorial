15eadeb0f862bb808057e42181abb0c84cac7c80
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-04-28 00:33:50 +1000

    Наброски работы с IPipelineBehavior

---
+++ MediatRWebApp/Infrastructure/UserIdPipe.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualBasic;
+
+namespace MediatRWebApp.Infrastructure
+{
+    public class UserIdPipe<TIn, TOut> : IPipelineBehavior<TIn, TOut>
+    {
+        private HttpContext _httpContext;
+        
+        public UserIdPipe(HttpContextAccessor accessor)
+        {
+            _httpContext = accessor.HttpContext;
+        }
+        
+        public Task<TOut> Handle(
+            TIn request, 
+            CancellationToken cancellationToken, 
+            RequestHandlerDelegate<TOut> next)
+        {
+            var userID = _httpContext.User.Claims
+                .FirstOrDefault(x => x.Type.Equals(ClaimTypes.NameIdentifier))
+                ?.Value;
+            
+            throw new System.NotImplementedException();
+        }
+    }
+}
